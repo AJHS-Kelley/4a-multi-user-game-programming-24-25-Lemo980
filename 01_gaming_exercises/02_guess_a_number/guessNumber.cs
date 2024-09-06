86ffe9734cddebb9b8b209797b187d12f2f9e625
EliotB <s11806270@students.duvalschools.org>
2024-09-06 13:58:34 -0400

    v0.0

---
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -0,0 +1,38 @@
+//Eliot Blanton, Guess A Number, v0.0, 9/6/24
+using System;
+
+class Template {
+    static void Main(){
+
+        int totalGuesses = 0;
+        int maxGuesses = 5;
+        
+
+        //Generate the secret number here
+
+        //while loop
+
+            //Allow user to guess number
+
+
+            //Compare guess to random number.
+
+
+            //If match, print win, if greater or less, give hint
+
+
+            //+1 to number of guesses made
+
+
+            //Check if the number of guesses made exceeds the max guesses
+
+
+            //If max guesses exceeded, print lose game message, if not, allow for another guess
+
+
+
+
+
+
+    }
+}
