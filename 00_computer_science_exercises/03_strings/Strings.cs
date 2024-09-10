02b76e631105d3a4dcfe2748da3ed36338b1f4d4
EliotB <s11806270@students.duvalschools.org>
2024-09-10 14:47:41 -0400

    Update Strings.cs

--- 00_computer_science_exercises/03_strings/Strings.cs
+++ 00_computer_science_exercises/03_strings/Strings.cs
@@ -17,16 +17,44 @@ class Strings {
 
     if(greeting.Length > goodbye.Length)
     {
-        Console.WriteLine("The greeting has more characters than the goodbye.")
+        Console.WriteLine("The greeting has more characters than the goodbye.");
     }
     else
     {
-        Console.WriteLine("The goodbye has more characters than the greeting.")
+        Console.WriteLine("The goodbye has more characters than the greeting.");
     }
 
     //Useful String Methods
-    Console.WriteLine(greeting.ToUpper) //STRING IS CONVERTED TO FULLY UPPERCASE
-    Console.WriteLine(greeting.ToLower) // string is converted to fully lowercase
+    Console.WriteLine(greeting.ToUpper()); //STRING IS CONVERTED TO FULLY UPPERCASE
+    Console.WriteLine(greeting.ToLower()); // string is converted to fully lowercase
+
+    //string concatenation
+    string comboString = greeting + goodbye;
+    Console.WriteLine(comboString);
+
+    //string concatenation method #2:
+    string comboString2 = string.Concat(greeting, goodbye);
+    Console.WriteLine(comboString2);
+
+    //string interpolation -- substituting variables into a string
+    string comboString3 = $"My greeting is {greeting}and my goodbye is {goodbye} \n";
+    Console.WriteLine(comboString3);
+
+    //string interpolation method #2
+    string comboString4 = "My greeting is " + greeting + "and my goodbye is " + goodbye;
+    Console.WriteLine(comboString4);
+
+    //Accessing parts of strings
+    //Index is a specific location of a character in a string
+    //ALL string indexes start at ZERO
+    Console.WriteLine(goodbye[0]); // print the character at index 0 (the first character in the string)
+    Console.WriteLine(goodbye[4]); // prints the FIFTH character in the list
+
+    //Where is it in my string?
+    Console.WriteLine(greeting.IndexOf("l"));
+
+
+
 
 
     }
