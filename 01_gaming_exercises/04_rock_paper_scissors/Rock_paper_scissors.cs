5b59f18810a2f52d1f6ea124bbbacbbefc3cabe8
EliotB <s11806270@students.duvalschools.org>
2024-09-16 14:45:18 -0400

    v0.5

--- 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.cs
@@ -3,15 +3,25 @@ using System;
 class Template {
     static void Main(){
     
-    str CPUchoice = ""
+    string CPUchoice = "";
+    string userChoice = "";
+    string userName = "";
+    int playerPts = 0;
+    int compPts = 0;
 
+    Console.WriteLine("To start, input your name and press ENTER.");
+    userName = Console.ReadLine();
+    Console.WriteLine("Welcome to rock paper scissors " + userName + "!\n");
+    Console.WriteLine("In this game two players (you and the CPU) choose rock, paper, or scissors.\nRock beats scissors, scissors beats paper, paper beats rock, and each round win will give you one point.\nFirst to 5 points wins the game!");
 
-
+    Console.WriteLine("Input rock, paper, or scissors");
+    userChoice = Console.ReadLine().ToLower();
+    //Console.WriteLine(userChoice);
 
 
     Random rnd = new Random();
     int Rand123 = rnd.Next(1, 4);
-    Console.WriteLine(Rand123);
+    //Console.WriteLine(Rand123);
 
     if(Rand123 == 1)
     {
@@ -25,12 +35,59 @@ class Template {
     {
         CPUchoice = "scissors";
     }
+    //Console.WriteLine(CPUchoice);
 
+    //If there is a draw
+    if(CPUchoice == userChoice)
+    {
+        Console.WriteLine("Draw. No points have been awarded");
+    }
 
+    //Checks for what the CPU chose then compares it to user choice and adds points accordingly
+    if(CPUchoice == "rock")
+    {
+        if(userChoice == "paper")
+        {
+            Console.WriteLine("Congrats " + userName + " you earned a point");
+            playerPts += 1;
+        }
 
+        if(userChoice == "scissors")
+        {
+            Console.WriteLine("You Lost! The Cpu gains a point");
+            compPts += 1;
+        }
+    }
 
+    else if(CPUchoice == "paper")
+    {
+        if(userChoice == "scissors")
+        {
+            Console.WriteLine("Congrats " + userName + " you earned a point");
+            playerPts += 1;
+        }
 
+        if(userChoice == "rock")
+        {
+            Console.WriteLine("You Lost! The Cpu gains a point");
+            compPts += 1;
+        }
+    }
 
+    else if(CPUchoice == "scissors")
+    {
+        if(userChoice == "rock")
+        {
+            Console.WriteLine("Congrats " + userName + " you earned a point");
+            playerPts += 1;
+        }
+
+        if(userChoice == "paper")
+        {
+            Console.WriteLine("You Lost! The Cpu gains a point");
+            compPts += 1;
+        }
+    }
 
 
 
