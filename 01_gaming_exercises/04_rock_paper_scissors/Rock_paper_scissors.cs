f715219f980016b464d5c97828b4b07e1f59b1e4
EliotB <s11806270@students.duvalschools.org>
2024-09-16 13:49:46 -0400

    Create Rock_paper_scissors.cs

---
+++ 01_gaming_exercises/04_rock_paper_scissors/Rock_paper_scissors.cs
@@ -0,0 +1,70 @@
+using System;
+
+class Template {
+    static void Main(){
+    
+    str CPUchoice = ""
+
+
+
+
+
+    Random rnd = new Random();
+    int Rand123 = rnd.Next(1, 4);
+    Console.WriteLine(Rand123);
+
+    if(Rand123 == 1)
+    {
+        CPUchoice = "rock";
+    }
+    else if(Rand123 == 2)
+    {
+        CPUchoice = "paper";
+    }
+    else
+    {
+        CPUchoice = "scissors";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
