998ebc9cf17fef6116cbef6f02d8bdff5aff6b97
EliotB <s11806270@students.duvalschools.org>
2024-10-30 13:08:30 -0400

    Update Template.cs

--- 00_computer_science_exercises/04_collections/Template.cs
+++ 00_computer_science_exercises/04_collections/Template.cs
@@ -1,9 +1,9 @@
-//Collections Example, Eliot Blanton, v0.0
+//Collections Example, Eliot Blanton, v0.1
 using System;
 using System.Linq;
 using System.Collections;
 
-class 04_Collections {
+class Collections {
     static void Main(){
 
         /*
@@ -18,9 +18,33 @@ class 04_Collections {
         */
 
         //DECLARING AND DEFINING AN ARRAY
-        string[] breakfastFoods = {Eggs, Waffle, Pancake, Bacon, Hashbrowns};
+        string[] breakfastFoods = {"Eggs", " Waffle", " Pancake", " Bacon", " Hashbrowns"};
         int[] testScores = {89, 12, 32, 99, 100};
         double[] GPA = {0.01D, 2.6D, 3.5D, 3.0D, 4.0D};
+
+        //Print Array Contents -- All elements on a single line
+        Console.WriteLine("The elements of each array are:\n");
+        Console.WriteLine("breakfastFoods: \n" + String.Join(",", breakfastFoods));
+        Console.WriteLine(); // print an empty line to the screen
+
+        //print array elements on seperate lines
+        Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
+        Console.WriteLine(); // print an empty line to the screen
+
+        Console.WriteLine("GPA: \n" + String.Join("\n", GPA));
+        Console.WriteLine(); // print an empty line to the screen
+
+        //How long is my array?   DETERMINING ARRAY LENGTH
+        Console.WriteLine("The length of the breakfast foods array is: " + breakfastFoods.Length);
+        //.Length is known as a property of the array
+        Console.WriteLine("The length of the GPA array is: " + GPA.Length);
+
+        //Accessing elements in the array within the index
+        Console.WriteLine("The first element in the array breakfastFoods is: " + breakfastFoods[0]);
+        // print the second element of the testScores array
+        Console.WriteLine("The second element in the array testScores is: " + testScores[1]);
+        // print the third element of the GPA array
+        Console.WriteLine("The third element in the array GPA is: " + GPA[2]);
 
 
 
